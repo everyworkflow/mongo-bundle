c3f9feea6c690e4ec381bd3629d66bd29c13eb6c
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:07:27 +0000

    Added ?ExtensionInterface

--- EveryWorkflowMongoBundle.php
+++ EveryWorkflowMongoBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\MongoBundle;
 
 use EveryWorkflow\MongoBundle\DependencyInjection\MongoExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowMongoBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new MongoExtension();
     }
